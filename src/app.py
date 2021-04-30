8c22ceddf684a4da944a5f6c443b401e76993a30
YogeshNain2015 <32641131+YogeshNain2015@users.noreply.github.com>
2021-04-30 11:51:06 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 
+
 def index():
+    
+    
     return "Hello ,World!"
     
+    
+    
 if __name__ == "__main__":
+    
+    
     app.run()
+    
+    
